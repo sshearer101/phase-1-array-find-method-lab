d6fa77ff80fa964236ec9a0880c493f89da7c255
Sam Shearer <sshearer101@gmail.com>
2021-08-01 10:04:05 -0600

    commit tree ef8a7d6f3e067aec2f312161a59edc762c3241eb

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
+function superbowlWin(record) {
+    const winner = record.find(winYear)
+    return (winner == undefined ? undefined : winner.year)
+}
+
+
+
+function winYear(year){
+    return year.result == "W"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function superbowlWin(record) {
+//     const winner = record.find(newFunction)
+//     return (winner == undefined ? undefined : winner.year)
+    
+// }
+
+
+// function newFunction(win){
+//     return win.result == 'W'
+// }
+
 
